d093f638ce6cbf4cf02f73adb4a16f65010317bc
zidaneho <zidaneho4567@gmail.com>
2026-01-02 09:52:48 -0800

    removed hangfire

--- SteamAlertsAPI/Program.cs
+++ SteamAlertsAPI/Program.cs
@@ -2,30 +2,30 @@ using Microsoft.EntityFrameworkCore;
 using SteamAlertsAPI.Controllers;
 using SteamAlertsAPI.Data;
 using SteamAlertsAPI.Services;
-using Hangfire;
-using Hangfire.SqlServer;
-using Hangfire.Dashboard;
-using Hangfire.PostgreSql;
+// using Hangfire;
+// using Hangfire.SqlServer;
+// using Hangfire.Dashboard;
+// using Hangfire.PostgreSql;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddHangfire(configuration => configuration
-    .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
-    .UseSimpleAssemblyNameTypeSerializer()
-    .UseRecommendedSerializerSettings()
-    .UsePostgreSqlStorage(options =>
-    {
-        options.UseNpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"));
-    }, new PostgreSqlStorageOptions
-    {
-        SchemaName = "steam_alerts_hangfire", // Custom schema name
-        PrepareSchemaIfNecessary = true,      // Automatically create tables
-        QueuePollInterval = TimeSpan.FromSeconds(15),
-        InvisibilityTimeout = TimeSpan.FromMinutes(5)
-    }));
+// builder.Services.AddHangfire(configuration => configuration
+//     .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+//     .UseSimpleAssemblyNameTypeSerializer()
+//     .UseRecommendedSerializerSettings()
+//     .UsePostgreSqlStorage(options =>
+//     {
+//         options.UseNpgsqlConnection(builder.Configuration.GetConnectionString("DefaultConnection"));
+//     }, new PostgreSqlStorageOptions
+//     {
+//         SchemaName = "steam_alerts_hangfire", // Custom schema name
+//         PrepareSchemaIfNecessary = true,      // Automatically create tables
+//         QueuePollInterval = TimeSpan.FromSeconds(15),
+//         InvisibilityTimeout = TimeSpan.FromMinutes(5)
+//     }));
 
-// 2. Add the processing server (this runs the background jobs)
-builder.Services.AddHangfireServer();
+// // 2. Add the processing server (this runs the background jobs)
+// builder.Services.AddHangfireServer();
 
 builder.Services.AddCors(options =>
 {
@@ -61,7 +61,7 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-app.UseHangfireDashboard();
+//app.UseHangfireDashboard();
 
 app.UseRouting();
 
@@ -71,11 +71,11 @@ app.UseHttpsRedirection();
 
 app.MapControllers();
 
-// Use the Cron expression for "every hour"
-RecurringJob.AddOrUpdate<ISteamService>(
-    "hourly-steam-fetch",
-    service => service.FetchAllMetricsAsync(),
-    Cron.Weekly);
+// // Use the Cron expression for "every hour"
+// RecurringJob.AddOrUpdate<ISteamService>(
+//     "hourly-steam-fetch",
+//     service => service.FetchAllMetricsAsync(),
+//     Cron.Weekly);
 
 using (var scope = app.Services.CreateScope())
 {
